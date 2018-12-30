88ffd5eaf356bda66a6f6196d8dbaa7c51d45c40
Yeh Shuyung <>
2018-12-30 09:58:16 +0800

    add cache function

--- pageman/region.py
+++ pageman/region.py
@@ -5,21 +5,36 @@ from element_wrapper import Element, ElementList
 class Region(object):
     def __init__(self, root):
         self._root = root
+        self._cache = dict()
 
-    def __cache__(self):
-        pass
+    def _set_cache(self, key, value):
+        self._cache[key] = value
 
-    def find_element(self, locator, element_class=None):
+    def find_element(self, locator, element_class=None, cacheable=True):
+        if locator in self._cache:
+            return self._cache[locator]
+
         _element = wait_element_presence(locator=locator, root=self._root, timeout=10)
+        _element_wrapper = None
         if element_class is not None:
-            return element_class(element=_element)
+            _element_wrapper = element_class(element=_element)
         else:
-            return Element(element=_element)
+            _element_wrapper = Element(element=_element)
+        if cacheable:
+            self._set_cache(locator, _element_wrapper)
+        return _element_wrapper
 
-    def find_elements(self, locator, element_class=None):
+    def find_elements(self, locator, element_class=None, cacheable=True):
+        if locator in self._cache:
+            return self._cache[locator]
+
         _elements = wait_all_elements_presence(locator=locator, root=self._root, timeout=10)
+        _elements_wrapper = None
         if element_class is not None:
-            return ElementList(elements=_elements, element_class=element_class)
+            _elements_wrapper = ElementList(elements=_elements, element_class=element_class)
         else:
-            return ElementList(elements=_elements)
+            _elements_wrapper = ElementList(elements=_elements)
+        if cacheable:
+            self._set_cache(locator, _elements_wrapper)
+        return _elements_wrapper
 
--- tests/test_region.py
+++ tests/test_region.py
@@ -18,10 +18,22 @@ class TestRegion(unittest.TestCase):
         mock_wait_presence.return_value = mock_element
         sut = self._init()
         actual = sut.find_element('<mock_locator>')
+        self.assertEqual(actual.get_element(), mock_element)
         actual_call = mock_wait_presence.call_args
         expected_call = mock.call(locator='<mock_locator>', root=self._mock_root, timeout=10)
-        self.assertEqual(actual.get_element(), mock_element)
         self.assertEqual(actual_call, expected_call)
+
+    @mock.patch('pageman.region.wait_element_presence')
+    def test_find_cached_element(self, mock_wait_presence):
+        mock_element = mock.Mock()
+        mock_wait_presence.return_value = mock_element
+        sut = self._init()
+        actual = sut.find_element('<mock_locator>')
+        self.assertEqual(actual.get_element(), mock_element)
+        cache = sut.find_element('<mock_locator>')
+        self.assertEqual(cache.get_element(), mock_element)
+        actual_call_count = len(mock_wait_presence.call_args_list)
+        self.assertEqual(actual_call_count, 1)
 
     @mock.patch('pageman.region.wait_all_elements_presence')
     def test_find_elements(self, mock_wait_all_presence):
@@ -29,10 +41,22 @@ class TestRegion(unittest.TestCase):
         mock_wait_all_presence.return_value = [mock_element]
         sut = self._init()
         actual = sut.find_elements('<mock_locator>')
+        self.assertEqual(actual.get_elements(), [mock_element])
         actual_call = mock_wait_all_presence.call_args
         expected_call = mock.call(locator='<mock_locator>', root=self._mock_root, timeout=10)
-        self.assertEqual(actual.get_elements(), [mock_element])
         self.assertEqual(actual_call, expected_call)
+
+    @mock.patch('pageman.region.wait_all_elements_presence')
+    def test_find_cache_elements(self, mock_wait_all_presence):
+        mock_element = mock.Mock()
+        mock_wait_all_presence.return_value = [mock_element]
+        sut = self._init()
+        actual = sut.find_elements('<mock_locator>')
+        self.assertEqual(actual.get_elements(), [mock_element])
+        cached = sut.find_elements('<mock_locator>')
+        self.assertEqual(cached.get_elements(), [mock_element])
+        actual_call_count = len(mock_wait_all_presence.call_args_list)
+        self.assertEqual(actual_call_count, 1)
 
 
 if __name__ == '__main__':
